c75dfd4fd4bc2f92a15cfc3636e61070b3e48fc3
Luiz Filipe da Silva <luizfilipe.tj@icloud.com>
2019-08-21 13:01:27 -0300

    Update carros.js

--- carros.js
+++ carros.js
@@ -1,7 +1,11 @@
 class Carro {
 	nome;
 	velocidadeAtual = 0;
-	velocidadeMaxima = 220;
+	velocidadeMaxima;
+	constructor(nome, velocidadeMaxima) {
+		this.nome = nome;
+		this.velocidadeMaxima = velocidadeMaxima || 220;
+	}
 	acelerar(valor) {
 		if (this.velocidadeAtual + valor <= this.velocidadeMaxima) {
 			this.velocidadeAtual += valor;
@@ -11,5 +15,15 @@ class Carro {
 		if (this.velocidadeAtual - valor >= 0) {
 			this.velocidadeAtual -= valor;
 		}
+	}
+}
+
+class Turbo extends Carro {
+	super(nome, velocidadeMaxima) {
+		this.nome = nome;
+		this.velocidadeMaxima = valocidadeMaxima;
+	};
+	turbo() {
+		this.velocidadeAtual = this.velocidadeMaxima;
 	}
 }
